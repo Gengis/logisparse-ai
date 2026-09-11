2481989456f85865fd34a159efb0c9d49be3860e
MarioMRdev <mrmartinezr47@gmail.com>
2026-09-10 22:18:33 -0500

    fix: eliminar carpeta temporal .next, eliminar ./globals.css del layout y cambios del frontend

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,7 @@
 import { Inter } from 'next/font/google';
 
-export const metadata = {
-    title: 'IA Soberana - Hackathon',
-    description: 'Procesamiento local seguro sin la nube',
-    manifest: '/manifest.json',
-};
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
